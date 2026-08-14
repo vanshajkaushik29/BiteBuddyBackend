1e36b857b36f00e650feba7884d405a14e0ad100
Vanshaj Kaushik <vanshajkaushik.mzn.7@gmail.com>
2026-08-15 02:06:46 +0530

    Initial order module completed

--- src/routes/orderRoutes.ts
+++ src/routes/orderRoutes.ts
@@ -1,12 +1,22 @@
 import express from "express";
-import {createOrder,getmyOrders} from "../controllers/orderController.js";
+import {createOrder,getmyOrders,tripOrders,getOrderById,updateOrder,cancelOrder} from "../controllers/orderController.js";
 import { protect } from "../middlewares/authMiddleware.js";
 
 const router = express.Router();
 
 router.use(protect);
 
+
 router.post("/", createOrder);
+
 router.get("/", getmyOrders);
+
+router.get("/trip/:tripId", tripOrders);
+
+router.get("/:id", getOrderById);
+
+router.patch("/:id", updateOrder);
+
+router.patch("/:id/cancel", cancelOrder);
 
 export default router;
